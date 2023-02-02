e81a99d72edaf27a02a2232f926cb0e507f454b8
AlbertoP231 <aprado1012@gmail.com>
2023-02-01 20:36:34 -0600

    Agregamos menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css'
 
+const Menu = () => (
+  <>
+  <p><a href='#home'>Inicio</a></p>
+  <p><a href='#wgpt3'>Qué es GPT?</a></p>
+  <p><a href='#possibility'>Open AI</a></p>
+  <p><a href='#features'>Casos de estudio</a></p>
+  <p><a href='#blog'>Biblioteca</a></p>
+  </>
+)
+
 //Metodologia BEM -> Block Elememt Modifier
 
-function Navbar() {
+const Navbar = ()  => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
@@ -14,11 +25,29 @@ function Navbar() {
           </div>
           </div>
           <div className='gpt3__navbar-links_container'>
-            <p><a href='#home'>Iicio</a></p>
-            <p><a href='#wgpt3'>Qué es GPT?</a></p>
-            <p><a href='#possibility'>Open AI</a></p>
-            <p><a href='#features'>Casos de Estudio</a></p>
-            <p><a href='#blog'>Biblioteca</a></p>
+            <Menu />
+          </div>
+          <div className='gpt3__navbar-sign'>
+            <p>Registrarse</p>
+            <button type='button'>Iniciar sesión </button>
+          </div>
+          <div className='gpt3__navbar-menu'>
+            {toggleMenu
+            ? <RiCloseLine  color='#fff' size={27} onClick={() => setToggleMenu(false)}/>
+            : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)}/>
+          }
+          {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              
+            </div>
+          </div>
+            
+          )
+
+          }
+
           </div>
         </div>  
        
