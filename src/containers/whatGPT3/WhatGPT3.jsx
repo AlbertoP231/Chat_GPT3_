ff11bb0f2ea0d5cf75e436c2cac69f14ebd6df6d
AlbertoP231 <aprado1012@gmail.com>
2023-02-03 00:40:03 -0600

    Cambios en WhatGPT solucionar error de bordes

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Feature } from '../../components'
 import './whatGPT3.css';
 
 
 const WhatGPT3 = () => {
     return (
-      <div>WhatGPT3</div>
+      <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+        <div className='gpt3__whatgpt3-feature'>
+          <Feature />   
+        </div>
+        <div className='gpt3__whatgpt3-header'>
+          <h1 className='gradient-text'>Las posibilidades están más allá de tu imaginación</h1>
+          <p>Explora La Biblioteca</p>
+        </div>
+        <div className='gpt3__whatgpt3-container'>
+          <Feature/>
+          <Feature/>
+          <Feature/>
+        </div>
+      </div>
     )
   }
   
