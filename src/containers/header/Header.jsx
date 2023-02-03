41da0cfab73551fdbed2790caaa470cd3d0f0343
AlbertoP231 <aprado1012@gmail.com>
2023-02-02 18:57:26 -0600

    Header terminado responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+
 
 
 const Header = () => {
     return (
-      <div>Header</div>
+      <div className='gpt3__header section__padding'>
+       <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Construyamos algo increíble con GPT-3 OpenAI</h1>
+        <p>Construyamos algo increíble con GPT-3
+            OpenIA
+            Sin embargo, cualquier cama para la indulgencia de asistencia en viaje es desagradable.
+            No todos los pensamientos ejercen bendición. 
+            Indulgencia camino todo alegría alteración bullicioso el apego. 
+            Fiesta que años para ordenar permitimos que se nos pida.
+            Su dirección de correo electrónico
+            Empezar
+            1.6k+
+            1.600 personas solicitaron acceso a una visita en las últimas 24 horas</p>
+
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Tu dirección de correo electrónico'/>
+          <button type='button'>Empezar</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people}  alt='people' />
+          <p>1.600 personas solicitaron acceso a una visita en las últimas 24 horas</p>
+        </div>
+       </div>
+       <div className='gpt3__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
     )
   }
   
