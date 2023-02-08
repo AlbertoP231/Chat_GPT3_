d57e7a02dbb38eda7dae400643bb9cbb7716e0f7
AlbertoP231 <aprado1012@gmail.com>
2023-02-07 22:03:30 -0600

    Whatpt3 y feature component al %70

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Feature } from '../../components'
+import  Feature  from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 
@@ -7,19 +7,19 @@ const WhatGPT3 = () => {
     return (
       <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
         <div className='gpt3__whatgpt3-feature'>
-          <Feature />   
+          <Feature title='¿Que es GPT-3?' text='Así que opinamos que mis amigos me envían un mensaje como un deleite. Todo el frente de la placa se escuchó oh debería. Su residencia defectuosa ni convencida propia. La conexión ha puesto los propios apartamentos imposibles bulliciosos. En el empalme, la señora insistió en que la humanidad él. Amistosa entrada de soltero a on by.'/>   
         </div>
-        <div className='gpt3__whatgpt3-header'>
-          <h1 className='gradient-text'>Las posibilidades están más allá de tu imaginación</h1>
+        <div className='gpt3__whatgpt3-heading'>
+          <h1 className='gradient__text'>Las posibilidades están más allá de tu imaginación</h1>
           <p>Explora La Biblioteca</p>
         </div>
         <div className='gpt3__whatgpt3-container'>
-          <Feature/>
-          <Feature/>
-          <Feature/>
+          <Feature title='Chatbots' text='Así que opinamos que mis amigos me envían un mensaje como un deleite. Todo el frente de la placa se escuchó oh debería. '/>
+          <Feature title='Base de conocimientos' text='En el empalme, la señora insistió en que la humanidad él. Amistosa entrada de soltero a on by. Como poner apartamentos propios imposibles b'/>
+          <Feature title='Educación' text='En el empalme, la señora insistió en que la humanidad él. Amistosa entrada de soltero a on by. Como poner apartamentos propios imposibles b'/>
         </div>
       </div>
     )
   }
   
-  export default WhatGPT3
+  export default WhatGPT3;
