102c570ad84052f7a22c0fa0405685d90c447fd8
AlbertoP231 <aprado1012@gmail.com>
2023-02-13 18:02:58 -0600

    Arreglamos estilo de texto

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,8 +4,10 @@ import './features.css';
 
 const Features = () => {
     return (
-      <div>
-        Features
+      <div className='gpt3__features section__padding' id='features'>
+        <div className='gpt3__features-heading'>
+          <h1 className='gradient__text'>El futuro es ahora y solo necesitas darte cuenta. Entra en el futuro hoy y haz que suceda.</h1>
+        </div>
       </div>
     )
   }
